816d69b079a2a0d65900284746484cb0e86935f8
john eblahan <64148013+warrrlock@users.noreply.github.com>
2023-10-10 00:02:53 -0400

    Laptime Display
    Laptimes are now recorded!
    
    -Player can view current laptime
    -Previous laps are displayed
    -Fastest laps are displayed.
    
    Eventually would like to have it so that all your laptimes are collected in a table. Perhaps by 5, and you can see it summarized in an end screen.

--- Assets/LapManager.cs
+++ Assets/LapManager.cs
@@ -6,30 +6,39 @@ using TMPro;
 public class LapManager : MonoBehaviour
 {
     public int totalLaps = 10;
+    public float lapTime;
+    public float prevLapTime;
+    public float fastestLap = float.MaxValue;
 
     private int currentLap = 0;
-    private float lapTime;
+
     private float startTime;
 
     public TMP_Text lapText;
+    public TMP_Text laptimeText;
+    public TMP_Text prevLapTimeText;
+    public TMP_Text fastLapText;
     public GameObject finishText;
 
     private void Start()
     {
+        StartNewLap();
         UpdateLapText();
     }
 
     private void Update()
     {
         lapTime += Time.deltaTime;
+
+        //show current laptime
+        string formattedTime = LapTimeFormat(lapTime);
+        laptimeText.text = "Time: " + formattedTime;    
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the entering object has the "player" tag
         if (other.CompareTag("Player"))
         {
-            // Assuming your start/finish line has a trigger collider
             UpdateLap();
             Debug.Log("Lap");
         }
@@ -38,10 +47,12 @@ public class LapManager : MonoBehaviour
     private void UpdateLap()
     {
         currentLap++;
+        UpdateLapTimes();
 
         if (currentLap <= totalLaps)
         {
             UpdateLapText();
+            StartNewLap();
         }
         else
         {
@@ -54,10 +65,56 @@ public class LapManager : MonoBehaviour
         lapText.text = "Lap: " + currentLap + "/" + totalLaps;
     }
 
+    private void UpdateLapTimes()
+    {
+        if (lapTime < fastestLap) //if u make a fastest lap
+        {
+            StartCoroutine(BlinkText());
+            fastestLap = lapTime; //set
+        }
+
+        //update other current & previous laps
+        prevLapTime = lapTime;
+        string prevLapFormatted = LapTimeFormat(prevLapTime);
+        prevLapTimeText.text = "P: " + prevLapFormatted;
+
+        string fastLapFormatted = (fastestLap == float.MaxValue) ? "0:00:00" : LapTimeFormat(fastestLap);
+        fastLapText.text = "F: " + fastLapFormatted;
+    }
+
     private void ShowFinishText()
     {
         Debug.Log("Finished");
         finishText.SetActive(true);
+    }
+
+    private void StartNewLap()
+    {
+        lapTime = 0f;
+        startTime = Time.time;
+    }
+
+    private string LapTimeFormat(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        int milliseconds = Mathf.FloorToInt((time * 1000) % 1000);
+
+        return string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+    }
+
+    private IEnumerator BlinkText()
+    {
+        WaitForSeconds blinkDuration = new WaitForSeconds(0.2f); // Adjust the blink duration as needed
+        int blinkCount = 5; // Adjust the blink count as needed
+
+        for (int i = 0; i < blinkCount; i++)
+        {
+            fastLapText.enabled = false;
+            yield return blinkDuration;
+            fastLapText.enabled = true;
+            yield return blinkDuration;
+        }
     }
 
 }
